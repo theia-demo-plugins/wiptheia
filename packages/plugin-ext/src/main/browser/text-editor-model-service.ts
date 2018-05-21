7d58c3995a4ab49dc5e6746d0f85d5e9f35c96a0
Yevhen Vydolob <yvydolob@redhat.com>
2018-05-21 11:53:41 +0300

    eclipse/che#9433 fix build
    Signed-off-by: Yevhen Vydolob <yvydolob@redhat.com>

--- packages/plugin-ext/src/main/browser/text-editor-model-service.ts
+++ packages/plugin-ext/src/main/browser/text-editor-model-service.ts
@@ -4,7 +4,7 @@
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  */
-import { Event, Emitter, DisposableCollection } from "@theia/core";
+import { Event, Emitter } from "@theia/core";
 import { MonacoEditorModel } from "@theia/monaco/lib/browser/monaco-editor-model";
 import { injectable, inject } from "inversify";
 import { MonacoTextModelService } from "@theia/monaco/lib/browser/monaco-text-model-service";
